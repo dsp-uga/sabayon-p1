e1fea7181fe49c87e205c1d85613f9661131cf2a
Marcus Hill <marcdh@uga.edu>
2019-02-02 10:53:50 -0500

    Created RDD of only the training files

--- src/NB.py
+++ src/NB.py
@@ -1,1 +1,21 @@
-#Initial Creation
+
+from pyspark import SparkContext
+import sys
+from os import path
+
+sc = SparkContext()
+data = sc.wholeTextFiles(sys.argv[1]) #'../../data/bytes'
+
+#Training Set
+fp = open('../dataset/files/X_small_train.txt')
+train_names = fp.read().split()
+file_path = 'file:' + path.realpath(sys.argv[1]) + '/'
+for i in range(len(train_names)):
+	train_names[i] = file_path + train_names[i] + '.bytes'
+train_names = sc.broadcast(train_names)
+
+#Training Labels
+fp = open('../dataset/files/y_small_train.txt')
+train_labels = sc.broadcast(fp.read().split())
+
+train_data = data.filter(lambda x: x[0] in train_names.value)
