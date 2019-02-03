0c44f8abf635033277a00a1879e63067462a9d27
Marcus Hill <marcdh@uga.edu>
2019-02-02 20:16:31 -0500

    Appended labels to training data

--- src/NB.py
+++ src/NB.py
@@ -5,12 +5,17 @@ from pyspark.sql.session import SparkSession
 import sys
 from os import path
 
+def append_train_label(document):
+	for i in range(len(train_names.value)):
+		if document[0] == train_names.value[i]:
+			return document[0], document[1], train_labels.value[i]
+
+#Spark Defaults
 sc = SparkContext()
 spark = SparkSession(sc)
 
-data = sc.wholeTextFiles('../../data/bytes') #sys.argv[1]
-
 #Training Set
+data = sc.wholeTextFiles('../../data/bytes') #sys.argv[1]
 fp = open('../dataset/files/X_small_train.txt')
 train_names = fp.read().split()
 file_path = 'file:' + path.realpath('../../data/bytes') + '/' #sys.argv[1]
@@ -21,6 +26,11 @@ train_names = sc.broadcast(train_names)
 #Training Labels
 fp = open('../dataset/files/y_small_train.txt')
 train_labels = sc.broadcast(fp.read().split())
+
+#Convert Training Data into a Data Frame
+train_data = data.filter(lambda x: x[0] in train_names.value)
+train_data = data.map(append_train_label)
+train_df = train_data.toDF(['id', 'text', 'label'])
 
 #Testing Set
 fp = open('../dataset/files/X_small_test.txt')
@@ -34,10 +44,7 @@ test_names = sc.broadcast(test_names)
 fp = open('../dataset/files/y_small_test.txt')
 test_labels = sc.broadcast(fp.read().split())
 
-#Convert Training Data into a Data Frame
-train_data = data.filter(lambda x: x[0] in train_names.value)
-train_df = train_data.toDF(['id', 'text'])
-
+#Convert Testing Data into a Data Frame
 test_data = data.filter(lambda x: x[0] in test_names.value)
 test_df = test_data.toDF(['id', 'text'])
 
@@ -49,5 +56,3 @@ training_freq = hashingTF.transform(training_words)
 idf = IDF(inputCol='freqs', outputCol='features')
 idf_model = idf.fit(training_freq)
 training_tfidf = idf_model.transform(training_freq)
-
-
