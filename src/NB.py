56c329d5ad3f32d870e88161bbeef8dcf27bef2c
Marcus Hill <marcdh@uga.edu>
2019-02-07 16:21:52 -0500

    Accuracy Evaluator add 1 adjustment

--- src/NB.py
+++ src/NB.py
@@ -1,7 +1,7 @@
 
 from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
-from pyspark.ml.feature import Tokenizer, HashingTF, IDF, StopWordsRemover, NGram
+from pyspark.ml.feature import Tokenizer, HashingTF, IDF, StopWordsRemover, NGram, CountVectorizer
 from pyspark.ml.classification import NaiveBayes
 from pyspark.ml import Pipeline
 import sys
@@ -17,12 +17,19 @@ def match_test_label(document):
 		if document[0] == test_names.value[i]:
 			return int(test_labels.value[i])
 
-def remove_line_id(document):
+def remove_train_line_id(document):
 	text = ''
 	for word in document[1].split():
 		if len(word) <= 2:
 			text += word + ' '
 	return document[0], text, document[2]
+
+def remove_test_line_id(document):
+	text = ''
+	for word in document[1].split():
+		if len(word) <= 2:
+			text += word + ' '
+	return document[0], text
 
 
 #Spark Defaults
@@ -45,7 +52,7 @@ train_labels = sc.broadcast(fp.read().split())
 #Convert Training Data into a Data Frame
 train_data = data.filter(lambda x: x[0] in train_names.value)
 train_data = train_data.map(append_train_label)
-train_data = train_data.map(remove_line_id)
+train_data = train_data.map(remove_train_line_id)
 train_df = train_data.toDF(['id', 'text', 'label'])
 
 #Testing Set
@@ -62,6 +69,7 @@ test_labels = sc.broadcast(fp.read().split())
 
 #Convert Testing Data into a Data Frame
 test_data = data.filter(lambda x: x[0] in test_names.value)
+test_data = test_data.map(remove_test_line_id)
 test_df = test_data.toDF(['id', 'text'])
 matched_test_labels = test_data.map(match_test_label).collect()
 
@@ -69,20 +77,26 @@ matched_test_labels = test_data.map(match_test_label).collect()
 tokenizer = Tokenizer(inputCol="text", outputCol="words")
 remover = StopWordsRemover(inputCol='words', outputCol='filtered', stopWords=['??', '00'])
 ngram = NGram(n=2, inputCol='filtered', outputCol='ngrams')
-hashingTF = HashingTF(inputCol="ngrams", outputCol="freqs", numFeatures=256)
-idf = IDF(inputCol='freqs', outputCol='features')
-nb = NaiveBayes(smoothing=1.0, modelType='multinomial')
+hashingTF = HashingTF(inputCol="filtered", outputCol="features") #, numFeatures=256)
+#countvec = CountVectorizer(inputCol='filtered', outputCol='features')
+#IDF
+#hashingTF = HashingTF(inputCol="ngrams", outputCol="freqs", numFeatures=256)
+#idf = IDF(inputCol='freqs', outputCol='features')
+nb = NaiveBayes(smoothing=1)
+
+
 
 #ML Pipeline Model
-pipeline = Pipeline(stages=[tokenizer, remover, ngram, hashingTF, idf, nb])
-#pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf, nb])
+pipeline = Pipeline(stages=[tokenizer, remover, ngram, hashingTF, nb])
+#pipeline = Pipeline(stages=[tokenizer, remover, countvec, nb])
 model = pipeline.fit(train_df)
+#model.save('NB_IDF')
 predictions = model.transform(test_df)
 
 #Evaluate Model Accuracy
 test_predictions = predictions.select('prediction').collect()
 correct = 0
 for i in range(len(test_predictions)):
-	if test_predictions[i][0] == matched_test_labels[i]:
+	if test_predictions[i][0]  + 1 == matched_test_labels[i]:
 		correct += 1
 print('NB Model Accuracy ', (correct / len(test_predictions)))
